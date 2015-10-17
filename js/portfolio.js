c194e978853dc48387b5dcbec45afa15d83b8182
Elise Kain <elise.kain@gmail.com>
2015-10-17 18:50:24 -0400

    Add smooth scrolling functionality

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,24 @@
-// Automatically set year for copyright in footer
-var year = new Date();
-year = year.getFullYear();
-document.getElementById("year").textContent = year;
+'use strict';
+
+$(document).ready(function() {
+
+  // Automatically set year for copyright in footer
+  var year = new Date();
+  year = year.getFullYear();
+  document.getElementById("year").textContent = year;
+
+  // Smooth Scrolling
+  $('a[href^="#"]').on("click",function (e) {
+  	    e.preventDefault();
+
+  	    var target = this.hash;
+  	    var $target = $(target);
+
+  	    $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top
+  	    }, 1100, 'swing', function () {
+  	        window.location.hash = target;
+  	    });
+  	});
+
+});
